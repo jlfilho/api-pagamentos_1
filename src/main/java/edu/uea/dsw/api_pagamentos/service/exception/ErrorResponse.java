f20fc3dc99749511d228271efb1edefa592e3636
João da Mata <jlfilho@users.noreply.github.com>
2025-03-12 21:17:52 -0400

    6-6: Implementar endpoints de pessoas e tratamento de exceções

--- src/main/java/edu/uea/dsw/api_pagamentos/service/exception/ErrorResponse.java
+++ src/main/java/edu/uea/dsw/api_pagamentos/service/exception/ErrorResponse.java
@@ -1,14 +1,16 @@
 package edu.uea.dsw.api_pagamentos.service.exception;
 
+import java.time.Instant;
+
 import lombok.Data;
 
 @Data
 public class ErrorResponse {
     private int status;
     private String message;
-    private long timestamp;
+    private Instant timestamp;
 
-    public ErrorResponse(int status, String message, long timestamp) {
+    public ErrorResponse(int status, String message, Instant timestamp) {
         this.status = status;
         this.message = message;
         this.timestamp = timestamp;
