691ddd70a4d408a640f2db797ff9e9a831117094
João da Mata <jlfilho@users.noreply.github.com>
2025-04-02 19:01:36 -0400

    Atualizaçã odo slq

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -33,5 +33,7 @@ INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, obser
 
 INSERT INTO usuarios (username, password) VALUES ('admin@uea.edu.br', '$2a$10$Ebmi/uPZlhTEB7e39gsPTOfADOsL0IdEcEQllZyogM/WI/WKUMYdW');
 
-INSERT INTO usuario_roles (usuario_id, role) VALUES (1, 'ROLE_USER');
+INSERT INTO usuarios (username, password) VALUES ('usuario@uea.edu.br', '$2a$10$Ebmi/uPZlhTEB7e39gsPTOfADOsL0IdEcEQllZyogM/WI/WKUMYdW');
+
 INSERT INTO usuario_roles (usuario_id, role) VALUES (1, 'ROLE_ADMIN');
+INSERT INTO usuario_roles (usuario_id, role) VALUES (2, 'ROLE_USER');*/
